0136bd670bc4736e206cec5b662cb06ab8112f4e
Jose Chery <Jcareergoals@hotmail.com>
2016-02-26 05:02:29 -0500

    added urls

--- server.js
+++ server.js
@@ -6,8 +6,37 @@ var fs = require('fs');
 var server = http.createServer(function(request, response){
 	// print the URL requested by the client
 	console.log('client requested URL: ', request.url); 
-	// urls will be added after this 
-
+	// valid URLs 
+	if(request.url === '/') {
+		fs.readFile('index.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'}); 
+			response.write(contents); 
+			response.end(); 
+		}); 
+	} else if(request.url === '/stylesheets/style.css') {
+		fs.readFile('/stylesheets/style.css', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/css'}); 
+			response.write(contents); 
+			response.end(); 
+		}); 
+	} else if (request.url === '/ninjas') {
+		fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'}); 
+			response.write(contents); 
+			response.end(); 
+		}); 
+	} else if (request.url === '/dojos/new') {
+		fs.readFile('/dojos/new.html', 'utf8', function(errors, contents){
+			response.writeHead(200, {'Content-Type': 'text/html'}); 
+			response.write(contents); 
+			response.end(); 
+		}); 
+	}
+	// if no URLS match
+	else {
+		response.writeHead(404); 
+		response.end('File not found!!!');
+	}
 }); 
 // assign port to server 
 server.listen(6789); 
